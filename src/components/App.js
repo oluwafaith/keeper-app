ae41778f7c9d4d6bd0330757691b9f993aa0aaba
Okpala Faith <okpalafaith1@gmail.com>
2020-11-04 22:13:44 +0100

    added the header and footer

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../../src/App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div >
+    <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
